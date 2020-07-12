1aedba55f5b5157432520da7136d93da1de931ee
Ali Aboutalebi <aboutalebi@live.com>
2020-07-12 15:15:43 +0430

    Centers list page established

--- Server/Controllers/PlaceController.cs
+++ Server/Controllers/PlaceController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Driver;
 using TciCommon.Models;
+using TciPM.Blazor.Shared;
 using TciPM.Classes;
 
 namespace TciPM.Blazor.Server.Controllers
@@ -14,11 +15,16 @@ namespace TciPM.Blazor.Server.Controllers
     {
         public PlaceController(ProvinceDBs dbs) : base(dbs) { }
 
-        [HttpGet]
         public ActionResult<List<Province>> ProvinceList()
         {
             return dbs.CommonDb.Find<Province>(_ => true).SortBy(p => p.Name).ToList();
         }
 
+        [Authorize(nameof(Permission.ShowCenters))]
+        public ActionResult<List<City>> CityList()
+        {
+            var province = dbs.CommonDb.FindFirst<Province>(p => p.Prefix == ProvincePrefix);
+            return db.Find<City>(c => c.Province == province.Id).SortBy(c => c.Name).ToList();
+        }
     }
 }
