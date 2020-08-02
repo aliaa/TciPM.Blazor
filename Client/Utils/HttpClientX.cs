038f316e6168d9f3a04983b4e8721e37c93fe2ff
Ali Aboutalebi <aboutalebi@live.com>
2020-08-02 17:30:18 +0430

    server side validation logic is done

--- Client/Utils/HttpClientX.cs
+++ Client/Utils/HttpClientX.cs
@@ -28,70 +28,81 @@ namespace TciPM.Blazor.Client
 
         public async Task<T> GetFromJsonAsync<T>(string requestUri, CancellationToken cancellationToken = default)
         {
+            HttpResponseMessage resp;
             try
             {
-                var resp = await GetAsync(requestUri, cancellationToken);
-                if (resp.StatusCode == System.Net.HttpStatusCode.Unauthorized)
-                {
-                    nav.NavigateTo("/login/out");
-                    return default(T);
-                }
-                else if ((int)resp.StatusCode >= 400)
-                    throw new HttpResponseException(await resp.Content.ReadAsStringAsync(), (int)resp.StatusCode);
-                return await resp.Content.ReadFromJsonAsync<T>(jsonOptions);
+                resp = await GetAsync(requestUri, cancellationToken);
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 throw new Exception("Errr on GET", ex);
             }
+            if (resp.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                nav.NavigateTo("/login/out");
+                return default;
+            }
+            else if ((int)resp.StatusCode >= 400)
+                throw new HttpResponseException(await resp.Content.ReadAsStringAsync(), (int)resp.StatusCode);
+            return await resp.Content.ReadFromJsonAsync<T>(jsonOptions);
         }
 
         public async Task<HttpResponseMessage> PostAsJsonAsync<T>(string requestUri, T value, CancellationToken cancellationToken = default)
         {
             JsonContent content = JsonContent.Create(value, mediaType: null, jsonOptions);
+            HttpResponseMessage resp;
             try
             {
-                var resp = await PostAsync(requestUri, content, cancellationToken);
-                if (resp.StatusCode == System.Net.HttpStatusCode.Unauthorized)
-                    nav.NavigateTo("/login");
-                else if ((int)resp.StatusCode >= 400)
-                    throw new HttpResponseException(await resp.Content.ReadAsStringAsync(), (int)resp.StatusCode);
-                return resp;
+                resp = await PostAsync(requestUri, content, cancellationToken);
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 throw new Exception("Errr on POST", ex);
             }
+            if (resp.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+                nav.NavigateTo("/login");
+            else if ((int)resp.StatusCode >= 400)
+                throw new HttpResponseException(await resp.Content.ReadAsStringAsync(), (int)resp.StatusCode);
+            return resp;
         }
 
-        public async Task<Res> PostAsJsonAsync<Req,Res>(string requestUri, Req value, CancellationToken cancellationToken = default)
+        public async Task<Res> PostAsJsonAsync<Req, Res>(string requestUri, Req value, CancellationToken cancellationToken = default)
         {
             JsonContent content = JsonContent.Create(value, mediaType: null, jsonOptions);
+            HttpResponseMessage resp;
             try
             {
-                var resp = await PostAsync(requestUri, content, cancellationToken);
-                if (resp.StatusCode == System.Net.HttpStatusCode.Unauthorized)
-                    nav.NavigateTo("/login");
-                else if ((int)resp.StatusCode >= 400)
-                    throw new HttpResponseException(await resp.Content.ReadAsStringAsync(), (int)resp.StatusCode);
-                return await resp.Content.ReadFromJsonAsync<Res>(jsonOptions);
+                resp = await PostAsync(requestUri, content, cancellationToken);
             }
             catch (Exception ex)
             {
                 throw new Exception("Errr on POST 2", ex);
             }
+
+            if (resp.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                nav.NavigateTo("/login");
+                return default;
+            }
+            else if ((int)resp.StatusCode >= 400)
+                throw new HttpResponseException(await resp.Content.ReadAsStringAsync(), (int)resp.StatusCode);
+            return await resp.Content.ReadFromJsonAsync<Res>(jsonOptions);
         }
 
         [Serializable]
         public class HttpResponseException : Exception
         {
             public int StatusCode { get; private set; } = -1;
-            public HttpResponseException(string message, int StatusCode) : base(message) 
+            public HttpResponseException(string message, int StatusCode) : base(message)
             {
                 this.StatusCode = StatusCode;
             }
 
             public HttpResponseException(string message, Exception inner) : base(message, inner) { }
+
+            public HttpResponseException() : base() { }
+
+            public HttpResponseException(string message) : base(message) { }
         }
     }
 }
