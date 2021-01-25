e77711dd8fe4d663414456daf9db15c0fc07d8c6
Ali Aboutalebi <abootalebi@gmail.com>
2021-01-25 14:21:32 +0330

    Add EdiUps page

--- Shared/Models/Ups.cs
+++ Shared/Models/Ups.cs
@@ -1,22 +1,30 @@
 ﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace TciPM.Blazor.Shared.Models
 {
     public class Ups : Equipment
     {
         [DisplayName("توان (KVA)")]
+        [Required]
         public int Power { get; set; }
 
         [DisplayName("تعداد باتری ها در هر سری")]
+        [Range(minimum:1, maximum:100)]
+        [Required]
         public int CellsCount { get; set; }
 
         [DisplayName("تعداد سری باتری ها")]
+        [Range(minimum:1, maximum:10)]
+        [Required]
         public int SeriesCount { get; set; }
 
         [DisplayName("ولتاژ هر سلول باتری")]
+        [Required]
         public float CellsNormalVoltage { get; set; }
 
         [DisplayName("ظرفیت باتری ها")]
+        [Required]
         public float BatteriesCapacity { get; set; }
 
         [DisplayName("نوع برق ورودی")]
